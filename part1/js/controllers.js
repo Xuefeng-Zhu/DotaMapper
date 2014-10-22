c53e1aa231fe06aecc5c63c5b24bff2c5ee04945
Xuefeng-Zhu <xzhu15@illinois.edu>
2014-10-22 00:06:47 -0500

    be able to add match

--- part1/js/controllers.js
+++ part1/js/controllers.js
@@ -7,44 +7,63 @@ var matchAPI = "http://limitless-falls-1957.herokuapp.com/match/";
 angular.module('myApp.controllers', []).
 controller('VisualCtrl', ['$scope', '$rootScope', '$http',
     function($scope, $rootScope, $http) {
-        $scope.data = [[]]
+        $scope.data = [
+            ['Vote']
+        ]
+        addMatch($rootScope.matchID);
+        $scope.matchID = "";
 
-        $http.get(matchAPI + $rootScope.matchID)
-            .then(function(res) {
-                var result = res.data.result;
-                var votes = result.positive_votes - result.negative_votes;
-                var players = result.players;
+        $scope.addMacth = function(){
+            if ($scope.matchID == ""){
+                return;
+            }
+            else{
+                addMatch($scope.matchID);
+                $scope.matchID = "";
+            }
+        }
 
-                var data = [
-                    ['Vote', 'Ratio']
-                ]
-                for (var i in players) {
-                    data.push([votes, players[i].kills - players[i].deaths])
-                }
+        function addMatch(id) {
+            $http.get(matchAPI + id)
+                .then(function(res) {
+                    var result = res.data.result;
+                    var votes = result.positive_votes - result.negative_votes;
+                    var players = result.players;
 
-                var d = google.visualization.arrayToDataTable(data);
-                var options = {
-                    hAxis: {
-                        title: 'Votes',
-                        minValue: -15,
-                        maxValue: 15
-                    },
-                    vAxis: {
-                        title: 'Kill/Death',
-                        minValue: -15,
-                        maxValue: 15
-                    },
-                    legend: 'none'
-                };
+                    $scope.data[0].push(""+id);
+                    for (var i = 1; i < $scope.data.length; i++){
+                        $scope.data[i].push(null);
+                    }
 
-                var chart = new google.visualization.ScatterChart(document.getElementById('visualization'));
+                    for (var i in players) {
+                        var temp = [votes]
+                        for (var j = 0; j < $scope.data[0].length - 2; j++){
+                            temp.push(null);
+                        }
+                        temp.push(players[i].kills - players[i].deaths);
+                        $scope.data.push(temp)
+                    }
 
-                chart.draw(d, options);
+                    var d = google.visualization.arrayToDataTable($scope.data);
+                    var options = {
+                        hAxis: {
+                            title: 'Votes',
+                            minValue: -15,
+                            maxValue: 15
+                        },
+                        vAxis: {
+                            title: 'Kill/Death',
+                            minValue: -15,
+                            maxValue: 15
+                        },
+                        legend: 'none'
+                    };
 
-                console.log(data);
-                console.log(res)
-            });
+                    var chart = new google.visualization.ScatterChart(document.getElementById('visualization'));
 
+                    chart.draw(d, options);
+                });
+        }
     }
 ])
     .controller('MainCtrl', ['$scope', '$rootScope', '$location',
